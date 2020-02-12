1e6063cb9bc438ddef78186e6839c8de41e9dbc0
Nauman Shigri <nauman.shigri@canalbaylabs.co>
2020-02-12 14:30:58 +0500

    add the index html and js, also add snipet for add all element of object

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('test');
+console.log('Snipte for Add all element of Objects');
+var a = [{ level: 1 }, { level: 2 }, { level: 3 }, { level: 6 }, { level: 4 }];
+console.log(
+	a.reduce(function(cnt, o) {
+		return cnt + o.level;
+	}, 0)
+);
